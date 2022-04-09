ba7ec6d3f1fe15a5b61060b4715f45d04b305a4d
Kelley42 <kelleykristine42@yahoo.com>
2022-04-09 18:15:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rock Paper Scissors Game
+
+This is a rock paper scissors game I made for a Udemy course.
+
+Instead of typing numbers for the choices, I decided to have users type the word out. To me this is just easier than having users remember what number goes with which word.
+
+## How to Play
+
+Pick rock, paper, or scissors to beat the computer. Rock beats scissors, scissors beats paper, and paper beats rock.
---
+++ rockpaperscissors.py
@@ -0,0 +1,102 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Defs
+def print_userchoice(userchoice):
+	global rock, paper, scissors
+	if userchoice == "rock":
+		print(rock)
+	elif userchoice == "paper":
+		print(paper)
+	elif userchoice == "scissors":
+		print(scissors)
+	else:
+		print("Incorrect choice - try again")
+
+
+def print_computerchoice(computerchoice):
+	if computerchoice == "rock":
+		print(f"Computer chooses: rock{rock}")
+	elif computerchoice == "paper":
+		print(f"Computer chooses: paper{paper}")
+	elif computerchoice == "scissors":
+		print(f"Computer chooses: scissors{scissors}")
+
+
+def playagain():
+	question = input("\nPlay again? ").lower()
+
+	questionyes = ["yes", "y"]
+	questionno = ["no", "n"]
+	
+	if any(match in question for match in questionyes):
+		game()
+	elif any(match in question for match in questionno):
+		exit()
+	else:
+		print("\nIncorrect response - try again!")
+		playagain()
+
+		
+def game():
+	userchoice = input("\nWhat do you choose? Rock, paper, or scissors? ").lower()
+	print_userchoice(userchoice)
+
+	seq = ["rock", "paper", "scissors"]
+	computerchoice = random.choice(seq)
+	print_computerchoice(computerchoice)
+
+	if computerchoice == "rock":
+		if userchoice == "rock":
+			print("It's a draw.")
+		elif userchoice == "paper":
+			print("You win!")
+		else:
+			print("You lost.")
+	elif computerchoice == "paper":
+		if userchoice == "rock":
+			print("You lost.")
+		elif userchoice == "paper":
+			print("It's a draw")
+		else:
+			print("You win!")
+	else:
+		if userchoice == "rock":
+			print("You win!")
+		elif userchoice == "paper":
+			print("You lose.")
+		else:
+			print("It's a draw.")
+	playagain()
+			
+# Start game
+print("Rock Paper Scissors Game!")
+
+game()
+
